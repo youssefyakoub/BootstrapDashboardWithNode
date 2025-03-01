b732b166013e093a22885cbdc223cec8cdb2fbca
youssefyakoub <youssefyakoub404@gmail.com>
2025-03-01 13:24:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require('dotenv').config();
+const path = require("path");
 const app = express();
 const port = process.env.PORT ||3000;
 const mongoose = require("mongoose");
