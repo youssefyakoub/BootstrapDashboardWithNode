837925db9206d7abfb5195a363039090f7b5a342
youssefyakoub <youssefyakoub404@gmail.com>
2025-03-01 11:31:47 +0200

    Bootstrap Dashboard

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT ||3000;
+const mongoose = require("mongoose");
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+var methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
+const allRoutes = require("./routes/allRoutes")
+const addUser = require("./routes/addUser")
+const editUser = require("./routes/editUser")
+
+// auto refresh
+
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
+
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+
+mongoose
+  .connect(
+    process.env.DB_CONNECTION
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+
+
+app.use(allRoutes)
+app.use("/user/add.html",addUser)
+app.use("/edit",editUser)
+
